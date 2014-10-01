ae8050a921e44144998f6d518ba26e9764da5c83
Delon Wong Her Laang <wongherlung@gmail.com>
2014-10-02 01:26:39 +0800

    Added new method in TareasIO for create empty file.

--- src/tareas/storage/TareasIO.java
+++ src/tareas/storage/TareasIO.java
@@ -1,10 +1,29 @@
 package tareas.storage;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TareasIO {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
+    }
+
+    public void createFile (String fileName) {
+        File file = new File("storage.txt");
+        boolean fileCreated = false;
+        try {
+            fileCreated = file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Error when creating file: " + e);
+        }
+
+        if (fileCreated) {
+            System.out.println("Empty file is created successfully: " + file.getPath());
+        } else {
+            System.out.println("Failed to created empty file: " + file.getPath());
+        }
     }
 
 }
