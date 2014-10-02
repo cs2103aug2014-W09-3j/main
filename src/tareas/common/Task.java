48bd00ec2182d62e09b11ecda29f92dc96964697
Delon Wong Her Laang <wongherlung@gmail.com>
2014-10-02 16:45:19 +0800

    Added get and set methods for all task attributes.

--- src/tareas/common/Task.java
+++ src/tareas/common/Task.java
@@ -47,11 +47,123 @@ public class Task {
 	// Color of task
 	private String color;
 	
-	Task () {
+	Task() {
 		// Set default values for recurrence
 		this.recurrence.put("frequency", null);
-		this.recurrence.put("Date", null);
-		this.recurrence.put("Day", null);
+		this.recurrence.put("date", null);
+		this.recurrence.put("day", null);
+	}
+	
+	public int getTaskID() {
+		return this.taskID;
+	}
+	
+	public void setTaskID(int id) {
+		this.taskID = id;
+	}
+	
+	public String getCategory() {
+		return this.category;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getDeadline() {
+		return this.deadline;
+	}
+	
+	public void setDeadline(String deadline) {
+		this.deadline = deadline;
+	}
+	
+	public String getStartDateTime() {
+		return this.startDatetime;
+	}
+	
+	public void setStartDateTime(String dateTime) {
+		this.startDatetime = dateTime;
+	}
+	
+	public String getEndDateTime() {
+		return this.endDateTime;
+	}
+	
+	public void setEndDateTime(String dateTime) {
+		this.endDateTime = dateTime;
+	}
+	
+	public String getRecurrenceFrequency() {
+		return this.recurrence.get("frequency");
+	}
+	
+	public void setRecurrenceFrequency(String freq) {
+		this.recurrence.put("frequency", freq);
+	}
+	
+	public String getRecurrenceDate() {
+		return this.recurrence.get("date");
+	}
+	
+	public void setRecurrenceDate(String date) {
+		this.recurrence.put("date", date);
+	}
+	
+	public String getRecurrenceDay() {
+		return this.recurrence.get("day");
+	}
+	
+	public void setRecurrenceDay(String day) {
+		this.recurrence.put("day", day);
+	}
+	
+	public ArrayList<String> getTags() {
+		return this.tags;
+	}
+	
+	public void addTag(String tag) {
+		this.tags.add(tag);
+	}
+	
+	public boolean isTaskCompleted() {
+		return this.completed;
+	}
+	
+	public void markTaskCompleted() {
+		this.completed = true;
+	}
+	
+	public boolean isTaskPriority() {
+		return this.priority;
+	}
+	
+	public void setTaskAsPriority() {
+		this.priority = true;
+	}
+	
+	public String getReminderDateTime() {
+		return this.reminderDateTime;
+	}
+	
+	public void setReminderDateTime(String dateTime) {
+		this.reminderDateTime = dateTime;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
 	}
 	
 }
