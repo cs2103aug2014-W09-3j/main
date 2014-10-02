d05d92475352f4c1a9fe4f918c2848507617b74c
Delon Wong Her Laang <wongherlung@gmail.com>
2014-10-02 21:48:14 +0800

    Added API to delete task.

--- src/tareas/storage/TareasIO.java
+++ src/tareas/storage/TareasIO.java
@@ -1,9 +1,17 @@
 package tareas.storage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import tareas.common.Task;
 import tareas.common.Tasks;
+
+/**
+ * @author Her Lung
+ *
+ * This class acts as the API for the Storage component.
+ */
 
 public class TareasIO {
 
@@ -18,14 +26,41 @@ public class TareasIO {
 		}
 	}
 	
-	public void insertTask(Task task) {
-		initialize();
+	private void write() {
 		StorageWriter writer = new StorageWriter();
-		allTasks.add(task);
 		try {
 			writer.write(allTasks);
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private void removeTaskFromArray(int id, ArrayList<Task> temp) {
+		Iterator<Task> iter = temp.iterator();
+		while(iter.hasNext()) {
+			Task task = iter.next();
+			if(task.getTaskID() == id) {
+				iter.remove();
+			}
+		}
+	}
+	
+	public void insertTask(Task task) {
+		initialize();
+		allTasks.add(task);
+		write();
+	}
+
+	public void deleteTask(int id) {
+		initialize();
+		if(id < 1 || id > allTasks.get().size()) {
+			// TODO: Add exception for Invalid ID.
+			System.out.println("Inavlid Task ID.");
+		} else {
+			ArrayList<Task> temp = allTasks.get();
+			removeTaskFromArray(id, temp);
+			allTasks.set(temp);
+			write();
 		}
 	}
 
