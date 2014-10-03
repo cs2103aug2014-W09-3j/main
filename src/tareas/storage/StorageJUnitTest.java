de642970bf991dfb21c3633199a249c83bb98fda
Delon Wong Her Laang <wongherlung@gmail.com>
2014-10-03 13:43:30 +0800

    Added unit test case for writing to file.

--- src/tareas/storage/StorageJUnitTest.java
+++ src/tareas/storage/StorageJUnitTest.java
@@ -3,16 +3,16 @@ package tareas.storage;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
 
 import tareas.common.*;
 
 public class StorageJUnitTest {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 	
 	@Test
@@ -23,6 +23,27 @@ public class StorageJUnitTest {
 		assertEquals(true, f.exists());
 	}
 	
+	@Test
+	public void testWriteToFile() throws IOException {
+		Tasks dummyTasks = generateDummyTasks();
+		StorageWriter writer = new StorageWriter();
+		writer.write(dummyTasks);
+		
+		StorageReader reader = new StorageReader();
+		Tasks result = reader.read();
+		assertEquals("task one", result.get().get(0).getDescription());
+	}
+	
+	public Tasks generateDummyTasks() {
+		Task task1 = new Task();
+		task1.setDescription("task one");
+		Task task2 = new Task();
+		task2.setDescription("task two");
+		Tasks allTasks = new Tasks();
+		allTasks.add(task1);
+		allTasks.add(task2);
+		return allTasks;
+	}
 	
 
 }
