ae42bbb311e85438ba3011ceace933581c92aac4
MrFool <lordyyap@gmail.com>
2014-10-06 03:55:17 +0800

    Added failing tests for actions that are supported for now

--- src/tareas/controller/ControllerTests.java
+++ src/tareas/controller/ControllerTests.java
@@ -17,13 +17,83 @@ import tareas.storage.*;
 public class ControllerTests {
 
     /**
-     * Controller test for adding a task
+     * Controller test for adding a floating task
+     */
+    @Test
+    public void addAFloatingTask() throws IOException {
+        executeCommand("buy milk");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for adding a floating tagged task
+     */
+    @Test
+    public void addAFloatingTaggedTask() throws IOException {
+        executeCommand("buy eggs -tag grocery");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for adding a timed task
+     */
+    @Test
+    public void addATimedTask() throws IOException {
+        executeCommand("meeting with 2103 group -from 22/09/2014 1200 -to 22/09/2014 2200");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for adding a timed task that stretches past a day
+     */
+    @Test
+    public void addATimedTaskMultipleDays() throws IOException {
+        executeCommand("family camping at sentosa -from 23/09/2014 0700 -to 25/09/2014 1200");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for adding a deadline task
+     */
+    @Test
+    public void addATimedTaskMultipleDays() throws IOException {
+        executeCommand("complete user guide -by 22/09/2014 1500");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for adding a recurring deadline daily task
      */
     @Test
     public void addATask() throws IOException {
-        executeCommand("buy milk");
+        executeCommand("walk the dog -recurring daily");
 
-        assertEquals(getTask(1), "buy milk");
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for adding a deadline recurring weekly task
+     */
+    @Test
+    public void addATask() throws IOException {
+        executeCommand("spa session -recurring weekly on monday");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for adding a deadline recurring monthly task
+     */
+    @Test
+    public void addATask() throws IOException {
+        executeCommand("family outing -recurring monthly on 13th");
+
+        //TODO add the right assertion
     }
 
     /**
@@ -32,42 +102,64 @@ public class ControllerTests {
     @Test
     public void editATask() throws IOException {
         executeCommand("buy milk");
-        executeCommand("");
+        executeCommand("-edit 1 -des buy pineapple");
 
-        assertEquals(getTask(1), "buy milk");
+        //TODO add the right assertion
     }
 
     /**
      * Controller test for deleting a task
      */
     @Test
-    public void deleteATask() throws IOException {
+    public void editATask() throws IOException {
         executeCommand("buy milk");
-        executeCommand("delete 1");
+        executeCommand("-delete 1");
 
-        assertEquals(getTask(1), "nothing");
+        //TODO add the right assertion
     }
 
     /**
-     * Controller test for tagging a task
+     * Controller test for searching a task through a string that the description contains
      */
     @Test
-    public void tagATask() throws IOException {
-        executeCommand("buy milk");
-        executeCommand("tag 1 groceries");
+    public void editATask() throws IOException {
+        executeCommand("buy watermelon");
+        executeCommand("-search watermelon");
 
-        assertEquals(getTask(1), "buy milk");
+        //TODO add the right assertion
     }
 
     /**
-     * Controller test for searching a task
+     * Controller test for searching a task through the task ID
      */
     @Test
-    public void searchATask() throws IOException {
-        executeCommand("buy milk");
-        executeCommand("search 1");
+    public void editATask() throws IOException {
+        executeCommand("buy watermelon");
+        executeCommand("-search 1");
 
-        assertEquals(getTask(1), "buy milk blah blah more details");
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for searching a task through the tasks' tag(s)
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("buy watermelon -tag grocery");
+        executeCommand("-search grocery");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for marking a task as done
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("buy watermelon");
+        executeCommand("-done 1");
+
+        //TODO add the right assertion
     }
 
     /**
@@ -76,9 +168,9 @@ public class ControllerTests {
     @Test
     public void undo() throws IOException {
         executeCommand("buy milk");
-        executeCommand("undo");
+        executeCommand("-undo");
 
-        assertEquals(getTask(1), "nothing");
+        //TODO add the right assertion
     }
 
     /**
@@ -86,9 +178,9 @@ public class ControllerTests {
      */
     @Test
     public void redo() throws IOException {
-        executeCommand("undo");
+        executeCommand("-undo");
 
-        assertEquals("failure");
+        //TODO add the right assertion
     }
 
     /**
@@ -97,10 +189,10 @@ public class ControllerTests {
     @Test
     public void redo() throws IOException {
         executeCommand("buy milk");
-        executeCommand("undo");
-        executeCommand("redo");
+        executeCommand("-undo");
+        executeCommand("-redo");
 
-        assertEquals(getTask(1), "buy milk");
+        //TODO add the right assertion
     }
 
     /**
@@ -109,8 +201,153 @@ public class ControllerTests {
     @Test
     public void redo() throws IOException {
         executeCommand("buy milk");
-        executeCommand("redo");
+        executeCommand("-redo");
 
-        assertEquals("failure");
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for postponing a task by hours
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("buy watermelon -by 22/09/2014 1500");
+        executeCommand("-postpone 1 -by 2 hours");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for postponing a task by specifying a whole new date
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("buy watermelon -by 22/09/2014 1500");
+        executeCommand("-postpone 1 -to 23/09/2014 1700");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for supporting custom done view
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("-view uncompleted");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for supporting custom today's view
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("-view today");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for supporting custom date view
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("-view 23/09/2014");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for prioritizing tasks
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("buy ham");
+        executeCommand("-prioritize 1");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for categorizing tasks
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("buy ham");
+        executeCommand("-categorize 1 -to groceries");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for setting reminders
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("buy ham");
+        executeCommand("-remind 3 -on 13/09/2014 1300");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for backing up
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("-backup");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for muting Tareas for days that one does not want to be disturbed or is sick etc.
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("-mute 12/09/2014 -to 15/09/2014");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for muting Tareas more natural command
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("-mute now -to 15/09/2014");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for changing font settings
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("-font Helvetica");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for changing font settings with size to cater to eyesight needs
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("-font Times New Roman -size 12");
+
+        //TODO add the right assertion
+    }
+
+    /**
+     * Controller test for colorizing tasks
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("-color 3 -with orange");
+
+        //TODO add the right assertion
     }
 }
