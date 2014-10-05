8bffb2dd33e1cfdfee8bdc250058833309489e74
MrFool <lordyyap@gmail.com>
2014-10-06 03:11:36 +0800

    Added even more failing test cases and added more skeletal stub code

--- src/tareas/controller/ControllerTests.java
+++ src/tareas/controller/ControllerTests.java
@@ -27,6 +27,50 @@ public class ControllerTests {
     }
 
     /**
+     * Controller test for editing a task
+     */
+    @Test
+    public void editATask() throws IOException {
+        executeCommand("buy milk");
+        executeCommand("");
+
+        assertEquals(getTask(1), "buy milk");
+    }
+
+    /**
+     * Controller test for deleting a task
+     */
+    @Test
+    public void deleteATask() throws IOException {
+        executeCommand("buy milk");
+        executeCommand("delete 1");
+
+        assertEquals(getTask(1), "nothing");
+    }
+
+    /**
+     * Controller test for tagging a task
+     */
+    @Test
+    public void tagATask() throws IOException {
+        executeCommand("buy milk");
+        executeCommand("tag 1 groceries");
+
+        assertEquals(getTask(1), "buy milk");
+    }
+
+    /**
+     * Controller test for searching a task
+     */
+    @Test
+    public void searchATask() throws IOException {
+        executeCommand("buy milk");
+        executeCommand("search 1");
+
+        assertEquals(getTask(1), "buy milk blah blah more details");
+    }
+
+    /**
      * Controller test for undoing an action
      */
     @Test
--- src/tareas/controller/TareasController.java
+++ src/tareas/controller/TareasController.java
@@ -14,25 +14,74 @@ import java.util.ArrayList;
 
 public class TareasController {
 
+    // Instantiate a TareasIO
     TareasIO tareas = new TareasIO();
 
+    // Keeping an ArrayList of states for both redoing and undoing
     ArrayList<Tasks> redoHistory = new ArrayList<Tasks>();
     ArrayList<Tasks> undoHistory = new ArrayList<Tasks>();
 
     public void executeCommand(String userInput) {
         TareasCommand command = TareasCommand.fromString(userInput);
+
+        switch (command) {
+            case ADD_COMMAND:
+                // TODO add correct method(s) to call
+            case EDIT_COMMAND:
+                // TODO add correct method(s) to call
+            case DELETE_COMMAND:
+                // TODO add correct method(s) to call
+            case SEARCH_COMMAND:
+                // TODO add correct method(s) to call
+            case DONE_COMMAND:
+                // TODO add correct method(s) to call
+            case UNDO_COMMAND:
+                // TODO add correct method(s) to call
+            case REDO_COMMAND:
+                // TODO add correct method(s) to call
+            case POSTPONE_COMMAND:
+                // TODO add correct method(s) to call
+            case VIEW_COMMAND:
+                // TODO add correct method(s) to call
+            case PRIORITIZE_COMMAND:
+                // TODO add correct method(s) to call
+            case CATEGORIZE_COMMAND:
+                // TODO add correct method(s) to call
+            case REMIND_COMMAND:
+                // TODO add correct method(s) to call
+            case BACKUP_COMMAND:
+                // TODO add correct method(s) to call
+            case MUTE_COMMAND:
+                // TODO add correct method(s) to call
+            case FONT_COMMAND:
+                // TODO add correct method(s) to call
+            case COLOR_COMMAND:
+                // TODO add correct method(s) to call
+            default:
+                // TODO throw a TareasException in that nothing is recognised?
+        }
     }
 
     public void undo() {
-
+        if (isAbleToUndo()) {
+            tareas.sendUndoState();
+            TareasGUI.sendUndoState();
+        } else {
+            TareasGUI.feedback("Nothing to undo.");
+        }
     }
 
     public void redo() {
-
+        if (isAbleToUndo()) {
+            tareas.sendRedoState();
+            TareasGUI.sendRedoState();
+        } else {
+            TareasGUI.feedback("Nothing to redo");
+        }
     }
 
     private boolean isAbleToRedo() {
-        if (history.isEmpty()) {
+        if (redoHistory.isEmpty()) {
             return false;
         } else {
             return true;
@@ -40,10 +89,18 @@ public class TareasController {
     }
 
     private boolean isAbleToUndo() {
-        if (history.isEmpty()) {
+        if (undoHistory.isEmpty()) {
             return false;
         } else {
             return true;
         }
+    }
+
+    private void addToUndoHistory(Tasks state) {
+        undoHistory.add(state);
+    }
+
+    private void addToRedoHistory(Tasks state) {
+        redoHistory.add(state);
     }
 }
