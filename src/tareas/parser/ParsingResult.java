e4b34ed5e8083c6f21ba8ba2febd2da5ca44305f
Kent <itaqmail@gmail.com>
2014-10-14 23:58:41 +0800

    created parsingresult

---
+++ src/tareas/parser/ParsingResult.java
@@ -0,0 +1,13 @@
+package tareas.parser;
+
+/**
+ * Represents a result from calling an API method in Parser.
+ * <p/>
+ * Created on Oct 14, 2014.
+ *
+ * @author Kent
+ */
+
+public class ParsingResult {
+
+}
