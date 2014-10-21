4a7abe6585d3366e922410cd52240f0498d05ed8
lareinating <lareinating@gmail.com>
2014-10-21 22:16:58 +0800

    testing insertTask

--- src/tareas/storage/StorageJUnitTest.java
+++ src/tareas/storage/StorageJUnitTest.java
@@ -11,10 +11,15 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class StorageJUnitTest {
-	
-	public static void main(String[] args) {
+	private StorageWriter writer = new StorageWriter();
+    private Tasks tasks = new Tasks();
+    private ArrayList<Task> newTasks;
+    private StorageReader reader = new StorageReader();
+    private TareasIO test = new TareasIO();
+    public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
+
 	
 	@Test
 	public void testCreateFile() {
@@ -26,28 +31,56 @@ public class StorageJUnitTest {
 	
 	@Test
 	public void testWriteToFile() throws IOException {
-		ArrayList<Task> dummyTasks = generateDummyTasks();
-		StorageWriter writer = new StorageWriter();
-		//writer.write(dummyTasks);
-		
-		StorageReader reader = new StorageReader();
+        Task task = new Task();
+
+        newTasks = tasks.get();
+        task.setDescription("task one");
+        newTasks.add(task);
+
+        writer.write(tasks);
+
 	    Tasks result = reader.read();
 		assertEquals("task one", result.get().get(0).getDescription());
 	}
 	
 	@Test
 	public void testDeleteTask() throws IOException {
-		ArrayList<Task> dummyTasks = generateDummyTasks();
-		dummyTasks.remove(1);
-		assertEquals(1, dummyTasks.size());
+        newTasks = tasks.get();
+
+        Task task2 = new Task();
+        newTasks.add(task2);
+        task2.setDescription("task two");
+        writer.write(tasks);
+
+//        TareasIO delete = new TareasIO();
+        test.deleteTask(1);
+
+        Tasks result = reader.read();
+		assertEquals("task two", result.get().get(0).getDescription());
 	}
 
     @Test
     public void testMassDelete() throws IOException {
-        ArrayList<Task> dummyTasks = generateDummyTasks();
-        dummyTasks.removeAll(dummyTasks);
-        assertEquals(0, dummyTasks.size());
+        test.massDelete();
+        Tasks result = reader.read();
+
+        assertEquals(true, result.get().isEmpty());
+        
     }
+
+    @Test
+    public void testInsertTask() throws IOException {
+        Task task3 = new Task();
+        task3.setDescription("task three");
+        test.insertTask(task3);
+
+        Tasks result = reader.read();
+
+        assertEquals("task three", result.get().get(0).getDescription());
+
+    }
+
+
 	
 	private ArrayList<Task> generateDummyTasks() {
 		Task task1 = new Task();
