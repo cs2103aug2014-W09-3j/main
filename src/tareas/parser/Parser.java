230695607cc24910d1410d0ab1731293c9084b06
Kent <itaqmail@gmail.com>
2014-10-23 03:41:16 +0800

    fix datetime

--- src/tareas/parser/Parser.java
+++ src/tareas/parser/Parser.java
@@ -1,6 +1,5 @@
 package tareas.parser;
 
-import tareas.common.Constants;
 import tareas.common.Log;
 
 import java.time.LocalDateTime;
@@ -15,54 +14,6 @@ import java.util.HashSet;
 
 public class Parser {
     private static String TAG = "tareas/parser";
-
-    public static void main1(String[] args) {
-        Constants.LOGGING_ENABLED = false;
-
-        String[] tests = {
-                //"buy ham",
-                //"meeting with 2103 group -from 22/09/2014 1200 -to 22/09/2014 2200",
-                //"complete user guide  -by 22/09/2014 1500",
-                //"gym -recurring daily from 0800 to 1000",
-                //"-edit 1 -des buy watermelon -start 22/09/2014 1200 -end 22/09/2014 1300",
-                //"-edit 3 -deadline 11/12/2015",
-                //"-delete 3",
-                //"-undo",
-                //"-font Helvetica -size 12",
-                //"",
-                "-what hello world", // unknown command
-                "buy ham -from 22/09/2014", // missing -to
-                "-edit 1 -dedline daily", // deadline spelled wrongly
-                "-edit -des something", // missing primary argument
-                "-delete", // same as above
-                "-redo xxx", // unexpected primary argument
-                "-delete 3 -random" // unexpected argument 'random'
-
-        };
-
-        for (String test : tests) {
-            if (test.equals("")) {
-                System.out.println();
-                continue;
-            }
-
-            // convert string test to TareasCommand
-            TareasCommand command = TareasCommand.fromString(test);
-
-            // get the parsing result
-            ParsingResult result = checkCommandValidity(command);
-
-            if (result.isSuccessful()) {
-                System.out.println("VALID COMMAND: " + command);
-            } else {
-                // use result.getStatus to get the error, result.getExtra will contain extra info (if any)
-                // associated with the error
-                System.out.println(String.format("%s: %s\n%s\n", result.getStatus(), test, result.getExtra()));
-            }
-        }
-
-        Constants.LOGGING_ENABLED = true;
-    }
 
     /**
      * Check whether a command matches any recognizable overload.
@@ -144,13 +95,4 @@ public class Parser {
         return LocalDateTime.parse(input.toUpperCase(), formatter);
     }*/
 
-    public static void main(String[] args) {
-        try {
-            System.out.println(getDateTimeFromString("14-03-17 14:10"));
-            //System.out.println(LocalTime.parse("4AM", DateTimeFormatter.ofPattern("Ha")));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-    }
 }
