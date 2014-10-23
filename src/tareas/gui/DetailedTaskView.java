5c15a801ac5c268087b587a0db7ed60112bbb4a7
Delon Wong Her Laang <wongherlung@gmail.com>
2014-10-23 21:48:40 +0800

    Removed OS window and made it draggable

--- src/tareas/gui/DetailedTaskView.java
+++ src/tareas/gui/DetailedTaskView.java
@@ -1,12 +1,15 @@
 package tareas.gui;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import tareas.common.Task;
 
 import java.io.IOException;
@@ -16,8 +19,10 @@ import java.util.ArrayList;
  * Created by Her Lung on 23/10/2014.
  */
 class DetailedTaskView {
-    MouseEvent event;
-    Task task;
+    private MouseEvent event;
+    private Task task;
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     protected DetailedTaskView(MouseEvent mouseEvent, Task task) {
         event = mouseEvent;
@@ -29,9 +34,26 @@ class DetailedTaskView {
         try {
             root = FXMLLoader.load(getClass().getResource("TasksDetailedView.fxml"));
             Stage stage = new Stage();
-            stage.setTitle("My New Stage Title");
+            stage.setTitle("Tasks Detailed View");
+            stage.initStyle(StageStyle.TRANSPARENT);
             stage.setScene(new Scene(root, 450, 450));
             stage.show();
+
+            // To allow the stage to be draggable
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+            root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    stage.setX(event.getScreenX() - xOffset);
+                    stage.setY(event.getScreenY() - yOffset);
+                }
+            });
 
             //hide this current window (if this is want you want
             //((Node)(event.getSource())).getScene().getWindow().hide();
@@ -46,7 +68,7 @@ class DetailedTaskView {
             FlowPane body = nodes.get(1);
             FlowPane footer = nodes.get(2);
 
-            System.out.println(header.getId());
+            header.getChildren().add(new Label("TEST"));
 
         } catch (IOException e) {
             e.printStackTrace();
